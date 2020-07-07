0a33cd47fe66c2ea9e65109508232782564771ed
Giorgi Coelho <giorgi_coelho@hotmail.com>
2020-07-07 15:41:45 -0300

    Enviando folder do primeiro projeto

---
+++ primeiro-projetoo/src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'meu-primeiro-component',
+    template: `
+        <p>Meu primeiro componente criado!</p>
+    `
+})
+export class MeuPrimeiroComponent { }
